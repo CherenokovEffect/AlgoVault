38ceb6e7660e328ad88679252655a384158d96ef
Maddy <142102472+PriestCoder@users.noreply.github.com>
2025-09-22 13:17:48 +0530

    Subset Generation

--- Recursion/GeneratingSubsets.cpp
+++ Recursion/GeneratingSubsets.cpp
@@ -1,7 +1,36 @@
 #include <bits/stdc++.h>
 
-using namespace std ;
+using namespace std;
 
-int main(){
-    
+vector<int> subset;
+
+void search(int k, int n)
+{
+    if (k == n)
+    {
+        if (subset.empty())
+        {
+            cout << "Φ";
+        }
+        for (auto it : subset)
+        {
+            cout << it << " ";
+        }
+        cout << "\n";
+    }
+    else
+    {
+        search(k + 1, n);
+        subset.push_back(k);
+        search(k + 1, n);
+        subset.pop_back();
+    }
+}
+
+int main()
+{
+
+    int k, n;
+    cin >> n;
+    search(0, n);
 }
